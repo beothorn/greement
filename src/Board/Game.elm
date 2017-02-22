452ad5260f610a7d33c4db88de5a717425210f7d
Lucas <lucass@gmail.com>
2017-02-22 20:27:31 -0300

    Make random tuples excluding list

--- src/Board/Game.elm
+++ src/Board/Game.elm
@@ -12,8 +12,31 @@ type alias GameConfig = {
     mountainCount : Int
 }
 
-distributeLand : Land -> Int -> List (List LandTile)
-distributeLand land qntd land =  List.length
+
+randomTuple : Int -> Int -> Random.Seed -> ((Int, Int), Random.Seed)
+randomTuple min max seed = 
+    let 
+        rgen = Random.int min max
+        (x, firstSeed) = Random.step rgen seed
+        (y, secondSeed) = Random.step rgen firstSeed
+    in
+    ((x, y), secondSeed)
+
+makeRandomTuplesExcluding : Int -> Int -> Int -> Random.Seed -> List (Int, Int) -> List (Int, Int)
+makeRandomTuplesExcluding min max qntd seed excluding = 
+    let
+        (rtuple, rseed) = randomTuple min max seed
+    in
+    case qntd of 
+        0 -> []
+        _ -> if List.member rtuple excluding 
+                then
+                    makeRandomTuplesExcluding min max qntd rseed excluding 
+                else 
+                    rtuple :: makeRandomTuplesExcluding min max (qntd - 1) rseed (rtuple :: excluding)
+
+makeRandomTuples : Int -> Int -> Int -> Random.Seed -> List (Int, Int)
+makeRandomTuples min max qntd seed = makeRandomTuplesExcluding min max qntd seed [] 
 
 makeBoard : GameConfig -> List String -> List (List LandTile)
 makeBoard gameConfig names = List.repeat gameConfig.size (List.repeat gameConfig.size {landType = Empty, owner = "NOONE"})
